a1fc1bdd6ad7d68dd9620e6f32b86377c90037c5
Herman Alexandrov <hermansochi@ya.ru>
2022-11-23 22:58:24 +0300

    Initial commit.

---
+++ test-app/app/Exceptions/InsufficientDataForDev.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class InsufficientDataForDev extends Exception
+{
+    /**
+     * Report or log an exception.
+     *
+     * @return void
+     */
+    public function report(): void
+    {
+        \Log::debug('Insufficient data for check deviation. Call method diffPrice before getDeviation.');
+    }
+}
